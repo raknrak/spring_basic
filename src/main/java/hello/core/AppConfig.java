3ca37bb7c7c9be12c2c9beef42cf5a11eaa0405f
Gyeongrak Choi <peter6617@gmail.com>
2023-12-23 22:44:16 +0900

    AppConfig.java Refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,17 +13,19 @@ public class AppConfig { // 애플리케이션의 실제 동작에 필요한 구
 
     //인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)
     // MemberServiceImpl -> MemoryMemberRepository
-    public MemberService memberService(){
+    // 리팩토링 전
+   /* public MemberService memberService(){
         return new MemberServiceImpl(new MemoryMemberRepository());
-    }
+    }*/
     //인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)
     // OrderServiceImpl -> MemoryMemberRepository, FixDiscountPolicy
-    public OrderService orderService(){
+// 리팩토링 전
+  /*  public OrderService orderService(){
         return new OrderServiceImpl(
                 new MemoryMemberRepository(),
                 new FixDiscountPolicy());
     }
-}
+}*/
 // AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
 // MemberServiceImpl
 // MemoryMemberRepository
@@ -35,3 +39,24 @@ public class AppConfig { // 애플리케이션의 실제 동작에 필요한 구
 // AppConfig 객체는 memoryMemberRepository 객체를 생성하고 그 참조값을 memberServiceImpl을 생성하면서 생성자로 전달한다.
 // 클라이언트인 memberServiceImpl 입장에서 보면 의존관계를 마치 외부에서 주입해주는 것 같다고 해서
 // DI(Dependency Injection) 우리말로 의존관계 주입 또는 의존성 주입이라고 한다.
+// --------------------------------------------------------
+// 리팩토링 후
+    //new MemoryMemberRepository() 의 중복 제거
+    //
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(
+                memberRepository(),
+                discountPolicy());
+
+    }
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+}
