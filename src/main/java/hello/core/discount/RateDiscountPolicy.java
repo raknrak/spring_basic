653c360dad5aa837fd1e60aec1b23018281686e8
Gyeongrak Choi <peter6617@gmail.com>
2023-12-23 18:47:39 +0900

    할인 방식 변경 및 적용 Test

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,21 @@
+package hello.core.discount;
+
+/*
+ * 정률 할인 정책 구현체
+ * */
+
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    private int discountPercent = 10;
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100; // 10퍼센트 할인 로직 -> 로직 구성 후 테스트 해보기.
+        }else {
+
+        } return 0;
+    }
+}
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -12,7 +13,15 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
     //회원과 주문 정책이 필요함
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    // 정액 할인 -> 정률 할인으로 정책 변경
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    // 인터페이스에만 의존하도록 의존관계를 변경
+    private DiscountPolicy discountPolicy; // 이 상태에서 바로 테스트시 NullpointException 발생
+    // 해결하기위해 클라이언트인 OrderServiceImpl에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해줘야함.
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
