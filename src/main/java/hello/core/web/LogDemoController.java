260c98e1fc65dae51ca8f3351718d1ee98a3ae4e
Gyeongrak Choi <peter6617@gmail.com>
2023-12-24 23:05:36 +0900

    request Scope Test

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,36 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import hello.core.logdemo.LogDemoService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
+
+//Caused by: org.springframework.beans.factory.support.ScopeNotActiveException:
+// Error creating bean with name 'myLogger':
+// Scope 'request' is not active for the current thread;
+// consider defining a scoped proxy for this bean if you intend to refer to it from a singleton
+//스프링 애플리케이션을 실행 시키면 오류가 발생한다. 메시지 마지막에 싱글톤이라는 단어가 나오고…
+//스프링 애플리케이션을 실행하는 시점에 싱글톤 빈은 생성해서 주입이 가능하지만, request 스코프 빈은 아직 생성되
+//지 않는다. 이 빈은 실제 고객의 요청이 와야 생성할 수 있다!
